89ed30e456e3a1fd2b0582fda60cf35cad6ade15
Gomacker <gomackerrst@gmail.com>
2023-08-23 16:09:02 +0800

    Upd

--- src/mode/CardMode.vue
+++ src/mode/CardMode.vue
@@ -14,16 +14,6 @@ console.log(`worldflipper loaded, Version: ${worldflipper.version}`)
 </script>
 
 <template>
-  <v-snackbar
-    v-model="worldflipper_updated"
-    color="green-lighten-4"
-    location="top"
-    style="top: 72px"
-    close-on-content-click
-  >
-    <v-icon icon="mdi-check-circle-outline" color="green" />
-    已更新数据：worldflipper
-  </v-snackbar>
   <router-view></router-view>
 </template>
 
