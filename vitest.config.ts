36e7dc81fdb0f217208dfc5fad38b202d31aadc3
Gomacker <gomackerrst@gmail.com>
2024-03-03 20:48:01 +0800

    Updates

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,15 +2,14 @@ import { fileURLToPath } from 'node:url'
 import { mergeConfig } from 'vite'
 import { configDefaults, defineConfig } from 'vitest/config'
 import viteConfig from './vite.config'
-import type { UserConfig } from "vitest";
 
 export default mergeConfig(
-  viteConfig as UserConfig,
+  viteConfig,
   defineConfig({
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
       root: fileURLToPath(new URL('./', import.meta.url))
     }
-  }) as UserConfig
+  })
 )
